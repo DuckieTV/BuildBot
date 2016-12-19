96a7f29b99ea0233d3af3ca343808b20de0956b1
SchizoDuckie <schizoduckie@gmail.com>
2016-12-19 23:11:55 +0100

    w.i.p. build and debtool

---
+++ build-deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+export PATH="${PATH}:/usr/local/bin/:/usr/bin"
+rm -rf /var/www/deploy/standalone
+rm -rf /var/www/deploy/newtab
+rm -rf /var/www/deploy/browseraction
+rm -rf /var/www/deploy/cordova
+rm -rf /var/www/deploy/osx
+rm -rf /var/www/deploy/binaries/*.*
+rm -rf /var/www/deploy/TMP
+rm -rf /var/www/Nightlies
+git clone git@github.com:SchizoDuckie/DuckieTV.git /var/www/Nightlies
+ln -s /var/www/DuckieTV/node_modules /var/www/Nightlies/node_modules
+cd /var/www/Nightlies
+git remote add nightly git@github.com:DuckieTV/Nightlies.git
+DTREV=$(date +"%Y%m%d")
+echo "set VERSION to ${DTREV}"
+echo "$DTREV" > /var/www/Nightlies/VERSION
+echo "Executing GULP"
+gulp
+gulp nightly
+echo "Committing changes"
+git add .
+git commit -m "Auto-Build: ${DTREV}"
+git tag -am "nightly-${DTREV}" "nightly-${DTREV}"
+echo "Pushing"
+git push nightly angular:master -f --tags
+rm -rf /var/www/deploy/TMP/
+echo "Building"
+cd /var/www/Nightlies/build/
+./pack.sh --all
+cd /var/www/deploy/
+zip -r "binaries/DuckieTV-${DTREV}-Chrome-BrowserAction.zip" browseraction
+zip -r "binaries/DuckieTV-${DTREV}-Chrome-NewTab.zip" newtab
---
+++ test-pack-windows.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export PATH="${PATH}:/usr/local/bin/:/usr/bin"
+rm -rf /var/www/deploy/standalone
+rm -rf /var/www/deploy/newtab
+rm -rf /var/www/deploy/browseraction
+rm -rf /var/www/deploy/cordova
+rm -rf /var/www/deploy/osx
+rm -rf /var/www/deploy/binaries/*.*
+rm -rf /var/www/deploy/TMP
+rm -rf /var/www/Nightlies
+cp -r /var/www/DuckieTV /var/www/Nightlies
+DTREV=$(date +"%Y%m%d")
+echo "set VERSION to ${DTREV}"
+echo "$DTREV" > /var/www/Nightlies/VERSION
+echo "Executing GULP"
+cd /var/www/Nightlies
+gulp
+gulp nightly
+rm -rf /var/www/deploy/TMP/
+echo "Building"
+cd /var/www/Nightlies/build/
+./pack.sh --windows
